1f9eb564b27e8071d30bcb6dd34fb1f65c1365cc
mauricio-rodriguez <mauricio.rodriguez@utec.edu.pe>
2022-07-06 14:44:52 -0500

    corrigiendo

---
+++ submit.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+
+#define dataType long long
+#define Matrix vector<vector<dataType>>
+
+using namespace std;
+
+void print(Matrix matrix){
+    for (auto row : matrix){
+        for (auto col : row){
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+}
+
+vector<dataType> getPrefix(vector<dataType> &A){
+    vector<dataType> p;
+    dataType sum = 0;
+    for (auto item : A){
+        p.push_back(item + sum);
+        sum+=item;
+    }
+    return p;
+}
+
+vector<dataType> getSecondaryPrefix(vector<dataType> &A){
+    vector<dataType> p;
+    dataType sum = 0, pos =1;
+    for (auto item : A){
+        p.push_back((item * pos) + sum);
+        sum+=item*pos++;
+    }
+    return p;
+}
+
+long S(vector<dataType> &p,dataType l, dataType r){
+    if(l==0) return p[r];
+    return p[r] - p[l-1];
+}
+
+long S2(vector<dataType> &p2,dataType l, dataType r){
+    if(l==0) return p2[r];
+    return p2[r] - p2[l-1];
+}
+
+long getCost(vector<dataType> &p, vector<dataType> &p2, dataType i, dataType j){
+    dataType m = (i+j)/2;
+    return S2(p2,i,m) - (i+1)*S(p,i,m) + (j+1)*S(p,m+1,j) - S2(p2,m+1,j);
+}
+
+long minPartition(Matrix &M,dataType n,vector<dataType> A,dataType k){
+    //get Sum of prefix 
+    vector<dataType> p = getPrefix(A);
+    vector<dataType> p2 = getSecondaryPrefix(A);
+
+    for (dataType i = 1; i <= n; i++){
+        dataType sum = 0;
+        //calculo de sumatoria 
+        M[i - 1][0] = getCost(p,p2,0,i-1);
+    }
+
+    for (dataType i = 1; i < n; i++){
+        for (dataType j = 1; j < k;j++){
+            M[i][j] = LLONG_MAX;
+            for (dataType l = j; l <= i; l++){
+                dataType cost = M[l][j-1] + getCost(p,p2,l,i);
+                M[i][j] = min(cost, M[i][j]); 
+            }   
+        }
+    }
+    return M[n-1][k-1]; 
+}
+
+int main(){
+    dataType k;
+    dataType n;
+    cin >> n;
+    vector<dataType> A(n);
+    for(dataType i = 0; i < n; i++) cin >> A[i];
+    k = n - 1;
+    Matrix matrix (n,vector<dataType>(k));
+    auto mn = minPartition(matrix,n,A,k);
+    for(dataType j = 1; j < n; j++){
+        cout << matrix[n-1][j-1] << " ";
+    }
+    cout << "\n";
+    //print(matrix);
+    return 0;
+
+}
+
+// int main(){
+//     vector<long> A = {10,20,30,40,50,60,70,80,90};
+//     long k  = 1;
+//     long n = 9;
+//     Matrix matrix (n,vector<long>(k));
+
+//     cout<<"resultao: "<<minPartition(matrix,n,A,k)<<endl;
+//     print(matrix);
+//     return 0;
+// }
