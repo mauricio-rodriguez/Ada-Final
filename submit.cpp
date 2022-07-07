cc3e442bd456fc8edbc3ef42095666f407742c4f
mauricio-rodriguez <mauricio.rodriguez@utec.edu.pe>
2022-07-06 21:31:30 -0500

    finally

--- submit.cpp
+++ submit.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <bits/stdc++.h>
-#include <fstream>
+#include <climits>
 
 #define dataType long long
 #define Matrix vector<vector<dataType>>
@@ -38,22 +37,22 @@ vector<dataType> getSecondaryPrefix(vector<dataType> &A){
     return p;
 }
 
-long S(vector<dataType> &p,dataType l, dataType r){
+dataType S(vector<dataType> &p,dataType l, dataType r){
     if(l==0) return p[r];
     return p[r] - p[l-1];
 }
 
-long S2(vector<dataType> &p2,dataType l, dataType r){
+dataType S2(vector<dataType> &p2,dataType l, dataType r){
     if(l==0) return p2[r];
     return p2[r] - p2[l-1];
 }
 
-long getCost(vector<dataType> &p, vector<dataType> &p2, dataType i, dataType j){
+dataType getCost(vector<dataType> &p, vector<dataType> &p2, dataType i, dataType j){
     dataType m = (i+j)/2;
     return S2(p2,i,m) - (i+1)*S(p,i,m) + (j+1)*S(p,m+1,j) - S2(p2,m+1,j);
 }
 
-long minPartition(Matrix &M,dataType n,vector<dataType> A,dataType k){
+dataType minPartition(Matrix &M,dataType n,vector<dataType> A,dataType k){
     //get Sum of prefix 
     vector<dataType> p = getPrefix(A);
     vector<dataType> p2 = getSecondaryPrefix(A);
@@ -62,7 +61,6 @@ long minPartition(Matrix &M,dataType n,vector<dataType> A,dataType k){
         //calculo de sumatoria 
         M[i - 1][0] = getCost(p,p2,0,i-1);
     }
-
     for (dataType j = 1; j < k;j++){
         for (dataType i = 0; i < n; i++){
             M[i][j] = LLONG_MAX;
